c5818d902e19dbea8a2b5b21b18434b4731f3ea3
rmfandyplayz <58617083+rmfandyplayz@users.noreply.github.com>
2026-01-31 18:01:14 -0700

    wait for eli to check shit

--- Assets/Scripts/BattleUIContract.cs
+++ Assets/Scripts/BattleUIContract.cs
@@ -2,199 +2,129 @@ using System;
 using System.Collections.Generic;
 
 // the "API" that the game logic and UI logic will communicate with
-namespace Shrimp5.UIContract
+namespace Sh.UIContract
 {
-    // expand only if necessary
-    public enum BattleUIMode
+    public enum BattleEventType
     {
-        ChoosingAction,
-        InspectingMove,
-        ChoosingSwitchTeammate,
-        ResolvingAction,
-        Paused
+        BattleWon,
+        BattleLost,
+        TurnStart, // switch to active character
+        CharacterDied,
+        StatusApplied,
+        StatusRemoved,
+        Heal,
+        TakeDamage,
+        Attack,
+        LogMessage, // flavor text probably
+        PlaySound,
+
+        // junk drawer
+        GenericEffect,
+        CameraShake
     }
 
-    // use ids instead of unity assets so UI and gameplay can work independently
-    // ui will map these ids to designated folders (Art/UI)
+
+    // fields can be null. fill in only what's necessary
+    // the one "god struct"
     [Serializable]
-    public struct HudData
+    public struct BattleEvent
     {
-        public string teammateName;
+        public BattleEventType eventType;
 
-        public int hp;
+        // who is involved
+        public string sourceId;    // i.e. shrimp.player.1
+        public string targetId;    // i.e. shrimp.enemy.3
+
+        // what happened
+        public string abilityId;   // "move.punch", "status.weaken", "move.heal", etc.
+        public string flavorText;  // "move.punch.effective", "move.heal", "move.status.apply", etc.
+
+        // the change and the truth
+        // use delta to change, and use final value so that ui won't be out of sync.
+        public int deltaValue;
+        public int finalValue;
+        public int maxValue;
+
+        // junk drawer
+        // pass in extra data if needed in case there are specific mechanics that do something "out of the ordinary"
+        public List<int> ints;         // i.e. multi hit move -- you tell ui how many times to hit. put 3 inside ints[0] (obv we have to coordinate on this)
+        public List<float> floats;     // i.e. play some animation at 1.5x the speed. put 1.5f inside floats[0]
+        public List<string> strings;   // i.e. trigger a specific sound file that isn't an id. for example, "sound.bonk" inside strings[0]
+    }
+
+
+    // battle initialization data (used only once)
+    [Serializable]
+    public struct BattleSetupData
+    {
+        public List<CharacterInitialData> playerTeam;
+        public List<CharacterInitialData> enemyTeam;
+    }
+
+    // also used only once
+    [Serializable]
+    public struct CharacterInitialData
+    {
+        public string name;
+        public string spriteId;
+        public string pfpId;
         public int maxHp;
-
+        public int currentHp;
         public int attack;
         public int attackSpeed;
+        public List<string> effects; // abilities, status effects
+    }
 
-        public string portraitIconID;  // i.e. "stallion_shrimp"
-        public List<List<string>> passives; // contains a list of status effects & abilites. as in, their icons and descriptions
-                                            // example: [ ["icons.statusEffects.weaken", "Weaken: Enemy's attack has been reduced by {cumulativeWeakenAmount}"],
-                                            // ["icons.statusEffects.slow", "Slow: Enemy's attack speed has been reduced by {cumulativeSlowAmount}"]]
 
-        public override string ToString()
+    public interface IBattleUI
+    {
+        void InitializeBattle(BattleSetupData setupData);
+        void QueueEvent(BattleEvent gameEvent);
+    }
+
+
+
+    // a dictionary of string names so typos are pretty much impossible.
+    // do not clone (u can't do that anyway lmao)
+    // add more stuff here when needed
+    public static class BattleKeys
+    {
+        public static class Animation
         {
-            return $"HUD DATA ==========\n" +
-                $"teammateName: {teammateName}\n" +
-                $"hp (currentHP): {hp}\n" +
-                $"maxHP: {maxHp}\n" +
-                $"attack: {attack}\n" +
-                $"attackSpeed: {attackSpeed}\n" +
-                $"portraitIconID: {portraitIconID}\n" +
-                $"passives count: {passives.Count}";
+            // examples:
+            // public const string ATTACK_BASIC = "anim_attack_basic";
+            // public const string HIT_REACTION = "anim_hit_reaction";
+            // public const string SPAWN_IN = "anim_spawn_in";
+            // public const string DEATH = "anim_death";
         }
-    }
 
-    // these count as one slot in the bottom row
-    [Serializable]
-    public struct ButtonData
-    {
-        public bool isEnabled; // enable a move? enable a teammate to be selected?
-        public string iconID;  // something like "move_basic_attack"
-        public string moveName;  // "Basic Attack" for example
-        public string moveShortDescription;  // "8 DMG, 20% to weaken by 50%"
-
-        // hint texts
-        public string hintText; // "Z to use, X to go back" lowkey useless as fuck
-
-        public override string ToString()
+        public static class SFX
         {
-            return $"BUTTON DATA =========\n" +
-                $"moveName: {moveName}\n " +
-                $"moveShortDescription: {moveShortDescription}\n" +
-                $"hintText: probably null\n" +
-                $"isEnabled: {isEnabled}\n" +
-                $"iconID: {iconID}";
+            // examples:
+            // public const string BONK = "sfx_bonk_01";
+            // public const string CRIT = "sfx_crit_impact";
+            // (that is, if we're using that list of strings in jumk drawer)
         }
-    }
 
-    [Serializable]
-    public struct InspectData // the stuff that displays when a player wants to learn more about a move
-    {
-        public string iconID;
-        public string title;
-        public string body;
-
-        public override string ToString()
+        public static class Params
         {
-            return $"INSPECT DATA ==========\n" +
-                $"iconID: {iconID}\n" +
-                $"title: {title} (note: probably unused)\n" +
-                $"body: {body}";
+            // examples:
+            // when event is "CameraShake"
+            // public const int CAM_SHAKE_INTENSITY_IDX = 0; // floats[0]
+            // public const int CAM_SHAKE_DURATION_IDX = 1;  // floats[1]
         }
-    }
 
-    [Serializable]
-    public struct TooltipData
-    {
-        public bool isVisible;
-        public string text;  // i.e. "Attack Speed is reduced by 6.7"
-
-        public override string ToString()
+        public static class Paths
         {
-            return $"TOOLTIP DATA ===========\n" +
-                $"isVisible: {isVisible}\n" +
-                $"text: {text}";
+            public const string UI_ROOT_PATH = "Art/UI/";
+            public const string SFX_ROOT_PATH = "Audio/SFX/";
         }
-    }
 
 
-    [Serializable]
-    public struct BattleSnapshot // the stuff that will be sent to UI code to re-render everything
-    {
-        public BattleUIMode battleMode;
-        public string promptText; // i.e. "your move!", "pick a shrimp"
-        public string flavorText; // i.e. "you used your mother, dealing 20 damage to the enemy, and healing yourself for 50."
-
-        public int selectedIndex; // a way to force the selection box to go to a certain option. NOTE: PROBABLY ISN'T USED
-
-        public HudData playerInfoData;
-        public HudData enemyInfoData;
-        
-        public List<ButtonData> buttons; // usually 4 max, but just in case
-
-        public InspectData inspectData;
-        
-        public TooltipData tooltipData;
-
-        public override string ToString()
+        public static class Sprites
         {
-            return $"BATTLE SNAPSHOT DATA as of {DateTime.Now} =========\n" +
-                $"battleMode: {battleMode}\n" +
-                $"promptText: {promptText}\n" +
-                $"flavorText: {flavorText}\n" +
-                $"selectedIndex: {selectedIndex}\n" +
-                $"playerInfoData: {playerInfoData.ToString()}\n" +
-                $"enemyInfoData: {enemyInfoData.ToString()}\n" +
-                $"tooltipData: {tooltipData.ToString()}\n" +
-                $"inspectData: {inspectData.ToString()}\n" +
-                $"button count: {buttons.Count}";
+            // example:
+            // public const string SPRITE_FROG = Paths.UI_ROOT_PATH + "shrimp.player.frog";
         }
-    }
-
-
-    // helpful for animations. a small event queue ui can utilize
-    public enum BattleUIEventType
-    {
-        ModeChanged,
-        HpChanged,
-        StatusAdded,
-        StatusRemoved,
-        CommandListChanged,
-        FlavorTextChanged,
-        AttackChanged,
-        AttackSpeedChanged
-    }
-
-
-    [Serializable]
-    public struct BattleUIEvent // see below for an explanation on what this is
-    {
-        public BattleUIEventType eventType;
-
-        public string target;
-        public string info;
-        public int oldValue;
-        public int newValue;
-    }
-    /*
-     * BattleUIEvent is a "hint" from gameplay to UI about what just happened.
-     * 
-     * this is NOT a source-of-truth data -- BattleSnapshot is.
-     * ui events exist only to help trigger animations, transitions, or
-     * give more effects to things (i.e. hp bar animations, panel slide, text pop)
-     * 
-     * if an event is ignored at all, ui should still render correctly via
-     * using the snapshot
-     * 
-     * what these fields mean:
-     * - target & info: they tell WHAT changed (player's HP, enemy's ATK)
-     * - oldValue & newValue: describes HOW it changed (player's HP goes from 50 -> 20)
-     */
-
-
-    public interface IBattleUIModel
-    {
-        event Action Changed;
-
-        // "what does the battle look like at this very moment?"
-        BattleSnapshot GetSnapshot();
-
-        // THIS IS MORE OPTIONAL IF ANYTHING: "what just happened since last time?"
-        // ui will call this after Changed to get potential animation cues
-        List<BattleUIEvent> DrainUIEvents();
-    }
-
-
-    public interface IBattleUIActions
-    {
-        void Confirm(int index); // confirm a selection (i.e. use a move) (z button)
-        void Secondary(int index); // the c button, which does different things under different contexts
-        void Back(); // x button
-        void PauseToggle();
-        void SetTooltipTarget(string tooltipID);
-
-        void DialogueConfirm(); // go to the next dialogue after the current one finished its animation (Z)
-        void DialogueSkipAll(); // skip all queued dialogue animations and go straight to the next event (C)
     }
 }
