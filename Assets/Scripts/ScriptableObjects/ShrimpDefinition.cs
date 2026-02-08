845eeeb616acb6b749e8891d7cb68ed6306dd92c
ocfrey <ocfrey270@gmail.com>
2026-02-08 13:27:11 -0700

    small scriptable object tweaks

--- Assets/Scripts/ScriptableObjects/ShrimpDefinition.cs
+++ Assets/Scripts/ScriptableObjects/ShrimpDefinition.cs
@@ -5,11 +5,12 @@ public class ShrimpDefinition : ScriptableObject
 {
     [Header("Identity")]           
     public string displayName;       
-
-    [Header("UI & Animation")]
-    public string shrimpSpriteID;           
-    public int maxHP;
     public string shrimpID;
+    
+    [Header("UI & Animation")]
+    public string shrimpSpriteID;    
+    public string pfpID;       
+    public int maxHP;
     public int baseSpeed;
     public int baseAttack;
     public AbilityDefinition ability;
